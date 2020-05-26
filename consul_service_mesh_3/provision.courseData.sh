5d13735a783eaa72abbfdb77aa017bcde17566f2
danielehc <daniele@hashicorp.com>
2020-05-26 18:43:31 +0200

    Provision scenario 3

--- consul_service_mesh_3/provision.courseData.sh
+++ consul_service_mesh_3/provision.courseData.sh
@@ -48,6 +48,25 @@ kubectl port-forward service/hashicorp-consul-ui 80:80 --address ${IP_ADDR} &
 
 log "Waiting for Consul pod to complete configuration."
 
+log "Deploying api backend"
+
+kubectl apply -f ~/api.yml
+
+log "Deploying web backend"
+
+kubectl apply -f ~/web.yml
+
+logs "Waiting for deployment to complete"
+
+until [ `kubectl get pods | grep -E "(web-|api-)deployment" | grep Running | wc -l ]
+do
+  sleep 5
+done
+
+export IP_ADDR=$(hostname -I | awk '{print $1}')
+
+kubectl port-forward service/web 9090:9090 --address ${IP_ADDR}
+
 finish
 
 EOFSRSLY
