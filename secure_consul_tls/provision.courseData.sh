6e0031f53b021b57981764fbf639e34f66929c77
danielehc <daniele@hashicorp.com>
2020-05-27 13:14:41 +0200

    Secure consul scenario

--- secure_consul_tls/provision.courseData.sh
+++ secure_consul_tls/provision.courseData.sh
@@ -16,15 +16,21 @@ apt-get install -y unzip curl jq > /dev/null
 
 log Install Consul locally
 
+# Retrieves lates version from checkpoint
+# Substitute this with APP_VERSION=x.y.z to configure a specific version.
 APP_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/consul | jq .current_version | tr -d '"')
 
-log Found Consul ${APP_VERSION}
+log Installing Consul ${APP_VERSION}
 
 curl -s https://releases.hashicorp.com/consul/${APP_VERSION}/consul_${APP_VERSION}_linux_amd64.zip -o consul_${APP_VERSION}_linux_amd64.zip
 
 unzip consul_${APP_VERSION}_linux_amd64.zip > /dev/null
 chmod +x consul
 mv consul /usr/local/bin/consul
+
+log Pulling Docker image for Consul ${APP_VERSION}
+
+docker pull consul:${APP_VERSION}
 
 finish
 
