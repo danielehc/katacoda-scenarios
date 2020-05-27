45ebdca92167feabab62406264c1cae63f6e611f
danielehc <daniele@hashicorp.com>
2020-05-27 12:54:07 +0200

    Secure consul scenario

--- secure_consul_tls/provision.courseData.sh
+++ secure_consul_tls/provision.courseData.sh
@@ -12,7 +12,14 @@ finish() {
 
 log Install Consul locally
 
+CHECKPOINT_URL="https://checkpoint-api.hashicorp.com/v1/check"
+APP_VERSION=$(curl -s "${CHECKPOINT_URL}"/consul | jq .current_version | tr -d '"')
 
+curl -s https://releases.hashicorp.com/consul/${APP_VERSION}/consul_${APP_VERSION}_linux_amd64.zip
+
+unzip consul_${APP_VERSION}_linux_amd64.zip 
+sudo chmod +x consul
+sudo mv consul /usr/local/bin/consul
 
 finish
 
