314ffea963ea173a6206055827e840a57f79e70a
danielehc <daniele@hashicorp.com>
2020-06-19 11:30:59 +0200

    Refactor service mesh scenarios

--- consul-service-mesh-2/provision.courseData.sh
+++ consul-service-mesh-2/provision.courseData.sh
@@ -37,7 +37,7 @@ done
 
 export IP_ADDR=$(hostname -I | awk '{print $1}')
 
-kubectl port-forward service/hashicorp-consul-ui 80:80 --address ${IP_ADDR} &
+kubectl port-forward service/hashicorp-consul-ui 80:80 --address ${IP_ADDR} > /tmp/forward_consul_ui.log 2>&1 &
 
 finish
 
