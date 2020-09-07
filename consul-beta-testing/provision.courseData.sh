723b0e90497aea525ebff9706844918b1eddd2e8
danielehc <daniele@hashicorp.com>
2020-09-08 00:58:48 +0200

    Consul Beta Testing

--- consul-beta-testing/provision.courseData.sh
+++ consul-beta-testing/provision.courseData.sh
@@ -66,82 +66,78 @@ docker run \
     consul agent \
      -node=client-1 \
      -join=${SERVER_IP} \
-     -config-file=/etc/consul.d/agent.hcl
+     -config-file=/etc/consul.d/agent.hcl \
+     -config-file=/etc/consul.d/svc-client.hcl
 
 docker run \
     -d \
     -v client_config:/etc/consul.d \
-    --name=api \
+    --name=backend-main \
     consul agent \
      -node=service-1 \
      -join=${SERVER_IP} \
-     -config-file=/etc/consul.d/agent.hcl
+     -config-file=/etc/consul.d/agent.hcl \
+     -config-file=/etc/consul.d/svc-main.hcl
 
 docker run \
     -d \
     -v client_config:/etc/consul.d \
-    --name=api \
+    --name=backend-clone \
     consul agent \
-     -node=service-1 \
+     -node=service-2 \
      -join=${SERVER_IP} \
-     -config-file=/etc/consul.d/agent.hcl
+     -config-file=/etc/consul.d/agent.hcl \
+     -config-file=/etc/consul.d/svc-clone.hcl
 
 
 log Starting Applications and configuring service mesh
 
+# Start applications
+# docker exec backend-main env LISTEN_ADDR=:9091 NAME=main fake-service
+# docker exec backend-clone env LISTEN_ADDR=:9092 NAME=clone fake-service
+# # docker exec client 
+
+# Start sidecar proxies
+# docker exec backend-main consul connect envoy -proxy-id backend-main-sidecar-proxy > /tmp/proxy.log &
+# docker exec backend-clone consul connect envoy -admin-bind=localhost:19001 -proxy-id backend-clone-sidecar-proxy > /tmp/proxy.log &
+# docker exec client consul connect envoy -admin-bind=localhost:19002 -proxy-id client-sidecar-proxy > /tmp/proxy.log &
+
+
 
 
 # log Download App 0.14.1
-
 # curl -s https://github.com/nicholasjackson/fake-service/releases/download/v0.14.1/fake-service-linux
 
-
 # log Install GO 1.5.1
-
 # curl -s https://golang.org/dl/go1.15.1.linux-amd64.tar.gz
-
 # tar -C /usr/local -xzf go1.14.3.linux-amd64.tar.gz
-
 # echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 
-
 # log Build Consul locally
-
 # # git clone git@github.com:hashicorp/consul.git
 # # cd consul
-
 # curl -s https://github.com/hashicorp/consul/archive/envoy-lb-v2.zip
-
 # git checkout envoy-lb-v2
 # make dev
 
-
 # log Install Consul locally
-
 # # Retrieves lates version from checkpoint
 # # Substitute this with APP_VERSION=x.y.z to configure a specific version.
 # APP_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/consul | jq .current_version | tr -d '"')
 
 # log Installing Consul ${APP_VERSION}
-
 # curl -s https://releases.hashicorp.com/consul/${APP_VERSION}/consul_${APP_VERSION}_linux_amd64.zip -o consul_${APP_VERSION}_linux_amd64.zip
-
 # unzip consul_${APP_VERSION}_linux_amd64.zip > /dev/null
 # chmod +x consul
 # mv consul /usr/local/bin/consul
-
 # rm -rf consul_${APP_VERSION}_linux_amd64.zip > /dev/null
 
 # log Pulling Docker image for Consul ${APP_VERSION}
-
 # docker pull consul:${APP_VERSION} > /dev/null
 
 # log Creating Docker volumes
-
 # docker volume create server_config > /dev/null
-
 # docker volume create client_config > /dev/null
-
 # docker container create --name volumes -v server_config:/server -v client_config:/client alpine > /dev/null
 
 finish
