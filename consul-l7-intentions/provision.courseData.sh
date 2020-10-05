059ccf1fdf3706ddcffd0e8a56569dfefc25a9d8
danielehc <daniele@hashicorp.com>
2020-10-05 15:03:33 +0200

    L7 intentions scenario

--- consul-l7-intentions/provision.courseData.sh
+++ consul-l7-intentions/provision.courseData.sh
@@ -84,7 +84,7 @@ docker run \
 log Starting Applications and configuring service mesh
 
 # Start applications
-docker exec counting env PORT=:9003 NAME=main counting-service > /tmp/service.log 2>&1 &
+docker exec counter env PORT=9003 NAME=main counting-service > /tmp/service.log 2>&1 &
 docker exec backend-clone env PORT=9002 COUNTING_SERVICE_URL="http://localhost:5000" dashboard-service > /tmp/service.log 2>&1 &
 
 # Start sidecar proxies
