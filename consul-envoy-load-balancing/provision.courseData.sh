03deedc94013087c7829cbcdf77c09fba6836b3d
danielehc <daniele@hashicorp.com>
2020-10-09 16:49:36 +0200

    Fix IGW config

--- consul-envoy-load-balancing/provision.courseData.sh
+++ consul-envoy-load-balancing/provision.courseData.sh
@@ -126,7 +126,7 @@ docker exec client consul connect envoy -admin-bind=localhost:19002 -proxy-id cl
 
 # Configure and start ingress gateway
 docker exec server consul config write /etc/consul.d/default.hcl
-docker exec server consul config write /etc/consul.d/igw-default.hcl
+# docker exec server consul config write /etc/consul.d/igw-default.hcl
 docker exec server consul config write /etc/consul.d/igw-backend.hcl
 docker exec ingress-gw consul connect envoy -gateway=ingress -register -service ingress-service -address '{{ GetInterfaceIP "eth0" }}:8888' > /tmp/proxy.log 2>&1 &
 
