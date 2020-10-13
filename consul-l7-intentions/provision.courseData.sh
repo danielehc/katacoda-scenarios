7a31d563e250e96b72dfcd0ab53f462155aa2539
danielehc <daniele@hashicorp.com>
2020-10-13 21:33:52 +0200

    Fix env

--- consul-l7-intentions/provision.courseData.sh
+++ consul-l7-intentions/provision.courseData.sh
@@ -102,7 +102,7 @@ log Starting Applications and configuring service mesh
 
 # Start applications
 docker exec counter sh -c "PORT=9003 NAME=main counting-service > /tmp/service.log 2>&1 &"
-docker exec dashboard sh -c "PORT=9002 COUNTING_SERVICE_URL="http://localhost:5000" dashboard-service > /tmp/service.log 2>&1 &"
+docker exec dashboard sh -c "PORT=9002 COUNTING_SERVICE_URL=\"http://localhost:5000\" dashboard-service > /tmp/service.log 2>&1 &"
 
 # Start sidecar proxies
 docker exec dashboard sh -c "consul connect envoy -sidecar-for dashboard > /tmp/proxy.log 2>&1 &"
