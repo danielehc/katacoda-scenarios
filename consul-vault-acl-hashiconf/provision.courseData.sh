690844c5df55ecb9841fe41d2ada78ba54787a7d
danielehc <daniele@hashicorp.com>
2021-05-12 18:03:15 +0200

    Adding vault ACL scenario

---
+++ consul-vault-acl-hashiconf/provision.courseData.sh
@@ -0,0 +1,34 @@
+cat << 'EOFSRSLY' > /tmp/provision.sh
+#! /bin/bash
+
+log() {
+  echo $(date) - ${@}
+}
+
+finish() {
+  touch /provision_complete
+  log "Complete!  Move on to the next step."
+}
+
+log "Install prerequisites"
+apt-get install -y unzip curl jq > /dev/null
+
+log Install Consul locally
+
+# Retrieves lates version from checkpoint
+# Substitute this with APP_VERSION=x.y.z to configure a specific version.
+APP_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/consul | jq .current_version | tr -d '"')
+
+log Installing Consul ${APP_VERSION}
+
+curl -s https://releases.hashicorp.com/consul/${APP_VERSION}/consul_${APP_VERSION}_linux_amd64.zip -o consul_${APP_VERSION}_linux_amd64.zip
+
+unzip consul_${APP_VERSION}_linux_amd64.zip > /dev/null
+chmod +x consul
+mv consul /usr/local/bin/consul
+
+finish
+
+EOFSRSLY
+
+chmod +x /tmp/provision.sh
